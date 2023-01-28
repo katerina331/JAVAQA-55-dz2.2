60cfc756ec29639d53acc25cfd64f9a977b239be
Ekaterina <katerina331-441@ya.ru>
2023-01-28 21:47:06 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 1200;
+              int bonus;
+              if (amount>1000) {
+                  bonus = amount/100;
+        } else {
+                bonus = 0;
+        }
+         System.out.println(amount + bonus);
+
+    }
+}
