efb40e200aeee11aeda2f339f1aa847ceebe6a15
Ekaterina <katerina331-441@ya.ru>
2023-01-29 19:47:48 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int amount = 1200;
-              int bonus;
-              if (amount>1000) {
-                  bonus = amount/100;
+        int bonus;
+        if (amount > 1000) {
+            bonus = amount / 100;
         } else {
-                bonus = 0;
+            bonus = 0;
         }
-         System.out.println(amount + bonus);
+        System.out.println(amount + bonus);
 
     }
 }
